0e36df73b98b12fcad595ce157a5d40d9874435c
lacarien <46967709+lacarien@users.noreply.github.com>
2019-02-05 21:21:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1717,7 +1717,7 @@ if(message.content === préfix + "FI défense"){
     var result = Math.floor((Math.random() * 100) + 1);
     if(result > 80)
     var y = Math.floor(Math.floor(y) * 2)
-        if(result < 80)
+        if(result > 80)
     var y = new Discord.RichEmbed()
     .addField("Parade infernal", "Andraaz fit une parade, il reçoit aucun dégats, et vous renvoie la moitié que vous lui avez infligez, impossible de bloquer")
     .setImage("https://www.wallpaperup.com/uploads/wallpapers/2018/03/17/1230961/bf3607bb70b902ac87145fd98bfda0c1-700.jpg")
