193316f84de3c3cd0e0826c1101059603e4f95bd
lacarien <46967709+lacarien@users.noreply.github.com>
2019-02-11 22:22:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2280,12 +2280,12 @@ if(message.content === préfix + "Shop"){
     message.channel.sendMessage(help_embed);
 }
     
-    if(message.content === préfix + "Potion"){
+   if(message.content === préfix + "Potion"){
     var help_embed = new Discord.RichEmbed()
     .addField("Potion d'HP faible'", "15 pièces de bronze")
     .addField("Potion d'HP", "30 pièces de bronze" + " \n 10 pièces d'argent")
     .addField("Potion d'HP haute", "75 pièces de bronze" + "\n 30 pièces d'argent" + "\n 10 pièces d'or \n \n \n")
-    .addBlankField( 3 )
+    .addField("Les potions de mana", "\n \n \n \n \n")
     .addField("Potion de mana", "15 pièces de bronze")
     .addField("Potion de mana", "30 pièces de bronze" + " \n 10 pièces d'argent")
     .addField("Potion de mana haute", "75 pièces de bronze" + "\n 30 pièces d'argent" + "\n 10 pièces d'or   ")
