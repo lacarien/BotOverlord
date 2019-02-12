2ba208c06b58f8e9efc3ee72913b473634b77819
lacarien <46967709+lacarien@users.noreply.github.com>
2019-02-12 20:56:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2312,11 +2312,11 @@ if(message.content === préfix + "Shop"){
     message.channel.sendMessage(help_embed);
 }
     
-    if(message.content === préfix + "Information sur l'argent"){
+if(message.content === préfix + "Information sur l'argent"){
     var help_embed = new Discord.RichEmbed()
     .addField("C'est quoi et à quoi il sert ?", "Il peut être obtenue après avoir tué un monstre ou bien lors de récompense des donjons. Il peut être utiliser pour de l'équipement, des demades à d'autre joueur, pour acheter des potions...")
     .addField("Où en trouver ?", "Il existe 3 types de pièces: \n \n Celui en bronze: que l'on trouve avant tout dans les marécages \n Celui en argent trouvable dans certain donjon et dans la forêt \n Celui en or: trouvable dans la montagne enneigé et contre des boss \n mais le moyen le plus facile d'en avoir c'est via des quêtes d'aventuriers")
-    .setFooter("Où acheter des items ?", "Au royaume dans la zone commercial)
+    .setFooter("Où acheter des items ?", "Au royaume dans la zone commercial")
     .setColor("#f0ff00")
     message.channel.sendMessage(help_embed);
 }
@@ -2338,23 +2338,7 @@ if(message.content === préfix + "Armure HP"){
     .setColor("#f0ff00")
     message.channel.sendMessage(help_embed);
 }
-if(message.content === préfix + "Armure mana"){
-    var help_embed = new Discord.RichEmbed()
-    .addField("Armure en tissus fin", "Prix : 70 pièces de bronze \n ?Achat armure en tissus fin")
-    .addField("Armure de sorcier traditionnel", "Prix : 150 pièces de bronze \n 45 pièces d'argent \n ?Achat armure de sorcier traditionnel")
-    .addField("Armure de sorcier reconnu", "Prix: 500 pièces de bronze \n 210 pièces d'argent \n 70 pièces d'or \ ?Achat armure de sorcier reconnu)
-    .setColor("#f0ff00")
-    message.channel.sendMessage(help_embed);
-}
 
-if(message.content === préfix + "Parchemin"){
-    var help_embed = new Discord.RichEmbed()
-    .addField("Les Parchemins", "Il est à savoir qu'un parchemin peut être utilisé par n'importe qui, après le prix diffère celon l'achat...")
-    .addField("Parchemin qui peut envoyer des messages à distance", "à voir avec le puriste")
-    .addField("En cours", "?En cours")
-    .setColor("#f0ff00")
-    message.channel.sendMessage(help_embed);
-}
     
     
     
