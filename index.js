662adf2c6d14f931a7d1dc2bec9920ca363713a0
lacarien <46967709+lacarien@users.noreply.github.com>
2019-02-24 20:42:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3008,7 +3008,7 @@ if(message.content === préfix + "Quête rang Or accomplie"){
     var maxss = Math.floor(Math.max(2)); 
     var q = Math.floor(Math.random() * (maxss - minss + 1) ) + minss;
     var y = new Discord.RichEmbed()
-    .addField("Gain de la récompense de la quête :exclamation: ",  ":third_place:  Pièce(s) de bronze: " + x + "\n :second_place:  Pièce(s) d'argent: " + q + "\n XP dans le rang OR" + y )
+    .addField("Gain de la récompense de la quête :exclamation: ",  ":third_place:  Pièce(s) de bronze: " + x + "\n :second_place:  Pièce(s) d'argent: " + q + "\n XP dans le rang Or: " + y )
     .setColor("#9e9a9a")
     message.channel.sendMessage(y);
 }
