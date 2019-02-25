3a893b628137d8a69190798efe2c69e56cd54795
lacarien <46967709+lacarien@users.noreply.github.com>
2019-02-25 16:20:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3037,7 +3037,7 @@ if(message.content === préfix + "Parade : " + w){
     var result = Math.floor((Math.random() * 100) + 1);
     var min = Math.floor(Math.min(10));
     var max = Math.floor(Math.max(30)); 
-    var w = Math.floor(Math.random() + (maxs - mins + 1) ) + mins;
+    var w = Math.floor(Math.random() + (max - min + 1) ) + min;
     if(result < 40)
     var w = new Discord.RichEmbed()
     .addField("Vous réussiez la parade et infligez de lourd dégats", y)
