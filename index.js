a1e9fb7b9372044bd8e4cdecd9108a44f3182230
lacarien <46967709+lacarien@users.noreply.github.com>
2019-04-10 08:34:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1289,13 +1289,12 @@ if(message.content === préfix + "Tir explosif"){
     var result = Math.floor((Math.random() * 100) + 1);
     if(result < 35)
     var y = Math.floor(Math.floor(y) * 1.2)
-        if(result >  35)
+        if(result <  35)
     var y = new Discord.RichEmbed()
-    
         .setColor("#12110F)
     .addField("Vous executez un blocage de faible qualité et réussisez à bloquer(-20 de Mana)", ":shield: ", y)
       .setImage("https://media.discordapp.net/attachments/536292650698014731/564852174803435540/latest.png?width=995&height=559")
-       if (result < 35)
+       if (result > 35)
     var y = new Discord.RichEmbed()
 .setColor("#12110F")
       .addField("Vous executez un blocage de faible qualité mais pourtant éfficace et réussisez à bloquer(-20 de mana)", ":shield: "+ y)
