7221c458cd1baec9083408ab1105fe54ec0892b5
lacarien <46967709+lacarien@users.noreply.github.com>
2019-06-22 17:57:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5998,7 +5998,7 @@ if(message.content === préfix + "Coup de dague rapide"){
    var max = Math.floor(Math.max(110)); 
    var y = Math.floor(Math.random() * (max - min + 1) ) + min;
       var result = Math.floor((Math.random() * 100) +1);
-   if(result < 60)
+   if(result < 70)
    var y = new Discord.RichEmbed()
    .addField("Vous lui infligé un coup de dague rapide et lui infligé", y)
    .setColor("#ff6000")
