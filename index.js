1eef4d02bcc0611b7798cde05444368e16900f4f
lacarien <46967709+lacarien@users.noreply.github.com>
2019-06-27 00:36:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6730,7 +6730,7 @@ if(message.content === préfix + "GS attaque"){
     
     
 
-    var groupe = Number(1 | 2)
+    var groupe = Number(1 & 2)
 
     if(message.content === préfix + "Récompenses " + groupe ){    
         var XPmin = Math.floor(Math.min(20));
