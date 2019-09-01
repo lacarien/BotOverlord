1ce5a9c052893099f0fa852bfa459671d6e8fab9
lacarien <46967709+lacarien@users.noreply.github.com>
2019-09-01 11:02:42 +0200

    Update UnderlordMenu.js

--- UnderlordMenu.js
+++ UnderlordMenu.js
@@ -30,7 +30,7 @@ if(message.content === préfix + "Vampire"){
 if(message.content === préfix + "Liche"){
     var help_embed = new Discord.RichEmbed()
     .setColor("#a65fe2")
-    .addField("Liche", "Vous êtes un ou une Liche, Vous avez une intéligence surdéveloppé, un vraie surdoué")
+    .addField("Liche", "Vous êtes une liche,  un ancien mage ayant tout sacrifié pour obtenir la vie éternelle par un état de ''Mort-vie''. Vous restez totalement conscient et surtout très intelligent.")
     .addField("Bonus", "Vous avez un bonus de 10% de mana et vous êtes immunisé au poison et au saignement (simple calcule rapide à faire, n'hésitez pas à contacter le staff si vous y arrivez pas il n'y a aucune honte !)")
     .addField("Malus", "Vous avez un roll de -40 en résistance physique et si vous prenez une autre classe que mage vous subirez un malus de -20% d'HP")
     .setImage("https://i.imgur.com/ZaA9j6e.jpg")
@@ -73,7 +73,7 @@ if(message.content === préfix + "Nymphe"){
     .setColor("#81b4bc")
     .setTitle("Nymphe")
     .setDescription("Vous êtes la représentation de ce qui pourrait se rapprocher à une déesse féminine. Vous êtes la plus belle")
-    .addField("Bonus", "10% de mana en plus et la capacité de faire rentrer en état de veille, venant à créer un esprit contrôlable qui peut se battre et qui a 1 HP")
+    .addField("Bonus", "10% de mana en plus et la capacité de faire rentrer en état de veille votre corps, venant à créer un esprit contrôlable qui peut se battre et qui a 1 HP")
     .addField("Malus", "Vous êtes obligatoirement une femme et votre éspèce est grandement recherché pour être vendue au marcher noir \n moins 20% d'HP")
     .setImage("https://media.discordapp.net/attachments/534758707561496580/585846279977041948/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.webp?width=373&height=560")
     message.channel.sendMessage(help_embed);
@@ -115,6 +115,106 @@ if(message.content === préfix + "Classe underlord"){
     .addField("Liste des races", "?Humain \n ?Vampire \n ?Beastman \n ?Démon \n ?Nymphe \n ?Elf \n ?Automaton \n ?Lézard \n Liche")
     message.channel.sendMessage(help_embed);
 }
+
+
+
+if(message.content === préfix + "Combattant"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#1a00ff")
+    .addField("Combattant", "Vous avez 20HP en plus")
+    .addField("Evolution lvl 15", "?Chevalier ou ?Paladin")
+    .addField("Liste d'attaque", "?Attaque combattant")
+    .addField("Bonus passifs", "?Passif combattant")
+    .setImage("https://vignette.wikia.nocookie.net/douze-salopards/images/a/ac/Elite-knight-cover_dark-souls_1364x1600_marked.jpg/revision/latest?cb=20171124095832&path-prefix=fr")
+    message.channel.sendMessage(help_embed);
+}
+
+if(message.content === préfix + "Voleur"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#ff0000")
+    .addField("Voleur", "Vous êtes une classe à faible HP mais qui fait de lourd dégats")
+    .addField("Evolution lvl 15", "?Assassin")
+    .addField("Liste d'attaque", "?Attaque voleur")
+    .addField("Bonus passifs", "?Passif voleur")
+    .setImage("https://vignette.wikia.nocookie.net/hai-to-gensou-no-grimgal/images/0/0c/Haruhiro%27s_anime_gallery.jpg/revision/latest/zoom-crop/width/320/height/320?cb=20160518071825")
+    message.channel.sendMessage(help_embed);
+}
+
+if(message.content === préfix + "Ranger"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#019521")
+    .addField("Ranger", "Vous êtes une classe qui attaque à distance faisant de gros dégats")
+    .addField("Evolution lvl 15", "?Archer")
+    .addField("Liste d'attaque", "?Attaque ranger")
+    .addField("Bonus passifs", "?Passif ranger")
+    .setImage("https://vignette.wikia.nocookie.net/overlordmaruyama/images/5/59/Overlord_EP07_015.png/revision/latest?cb=20150822091615")
+    message.channel.sendMessage(help_embed);
+}
+
+
+if(message.content === préfix + "Mage"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#00ffe7")
+    .addField("Mage", "Vous avez 20HP en plus")
+    .addField("Evolution lvl 15", "?Sorcier \n Les Nymphes et les Elfs peuvent evoluer en ?Druide")
+    .addField("Liste d'attaque", "?Attaque mage")
+    .addField("Bonus passifs", "?Passif mage")
+    .setImage("https://static.zerochan.net/Evileye.%28Overlord%29.full.2317125.png")
+    message.channel.sendMessage(help_embed);
+}
+
+/// LISTE D'ATTAQUE PALIER 1 ///
+/// LISTE D'ATTAQUE PALIER 1 ///
+/// LISTE D'ATTAQUE PALIER 1 ///
+
+
+
+if(message.content === préfix + "Attaque combattant"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#1a00ff")
+    .addField("LVL 1", "?Coup d'épée + ?Blocage simple")
+    .addField("LVL 7", "?Combattant ultime")
+    .addField("LVL 12 si vous voudrez évoluer en tant que chevalier", "?C Coup d'épée")
+    .addField("LVL 12 si vous voudrez évoluer en tant que paladin", "?Blocage supérieure")
+    message.channel.sendMessage(help_embed);
+}
+
+if(message.content === préfix + "Attaque mage"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#00ffe7")
+    .addField("LVL 1", "?M Blocage simple + ?Magie simple")
+    .addField("LVL 4", "?Soins de bas rang")
+    .addField("LVL 9", "?Mage ultime")
+    .addField("LVL 12 si vous voudrez évoluer en tant que Sorcier", "?Sort simple")
+    .addField("LVL 12 si vous voudrez évoluer en tant que Sorcier", "?Sort naturel")
+    message.channel.sendMessage(help_embed);
+}
+
+if(message.content === préfix + "Attaque ranger"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#019521")
+    .addField("LVL 1", "?Tire à l'arc + ?R Blocage simple")
+    .addField("LVL 4", "Tire rapide")
+    .addField("LVL 9", "?Ranger ultime")
+    .addField("LVL 12", "?Blocage rapide")
+    message.channel.sendMessage(help_embed);
+}
+
+if(message.content === préfix + "Attaque voleur"){
+    var help_embed = new Discord.RichEmbed()
+    .setColor("#ff0000")
+    .addField("LVL 1", "?Coup d'épée + ?V Blocage simple")
+    .addField("LVL 4", "?Coups bas")
+    .addField("LVL 9", "?Voleur ultime")
+    .addField("LVL 12", "?Esquive")
+    message.channel.sendMessage(help_embed);
+}
+
+/// PASSIFS PALIER 1 ///
+/// PASSIFS PALIER 1 ///
+/// PASSIFS PALIER 1 ///
+
+
 
 
 
