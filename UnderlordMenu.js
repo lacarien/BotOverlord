bcbe8ad7168ff448d00ac0aa2a617e84fd57b1da
lacarien <46967709+lacarien@users.noreply.github.com>
2019-09-02 10:49:33 +0200

    Update UnderlordMenu.js

--- UnderlordMenu.js
+++ UnderlordMenu.js
@@ -1,3 +1,4 @@
+
 if(message.content === préfix + "Help underlord"){
     var help_embed = new Discord.RichEmbed()
     .setColor("#ffffff")
@@ -13,6 +14,7 @@ if(message.content === préfix + "Humain"){
     .setColor("#939696")
     .setTitle("Humain")
     .setDescription("Vous êtes un simple humain banale, c'est con dit comme ça mais...C'est bien le cas")
+    .addField("Coups spéciaux", "?CS Humain")
     .setImage("https://vignette.wikia.nocookie.net/overlord8371/images/d/dc/Blue_Rose.jpg/revision/latest?cb=20180803234950&path-prefix=fr")
     message.channel.sendMessage(help_embed);
 }
@@ -23,6 +25,7 @@ if(message.content === préfix + "Vampire"){
     .addField("Vampire", "Vous êtes un ou une vampire, vous avez des capacités uniques qui se débloquent au fils du temps")
     .addField("Bonus", "Vous avez la possibilité de transformer une seule personne(humaine) en vampire tant qu'il soit inférieure à votre lvl. Ce dernier sera considéré comme votre serviteur et il ne pourra pas vous tuer bloqué par sa nouvelle nature")
     .addField("Malus", "Vous avez 10% d'HP en moins (simple calcule rapide à faire, n'hésitez pas à contacter le staff si vous y arrivez pas il n'y a aucune honte !)")
+    .addField("Coups spéciaux", "?CS Vampire")
     .setImage("https://img-9gag-fun.9cache.com/photo/a47NvYd_700b.jpg")
     message.channel.sendMessage(help_embed);
 }
@@ -33,6 +36,7 @@ if(message.content === préfix + "Liche"){
     .addField("Liche", "Vous êtes une liche,  un ancien mage ayant tout sacrifié pour obtenir la vie éternelle par un état de ''Mort-vie''. Vous restez totalement conscient et surtout très intelligent.")
     .addField("Bonus", "Vous avez un bonus de 10% de mana et vous êtes immunisé au poison et au saignement (simple calcule rapide à faire, n'hésitez pas à contacter le staff si vous y arrivez pas il n'y a aucune honte !)")
     .addField("Malus", "Vous avez un roll de -40 en résistance physique et si vous prenez une autre classe que mage vous subirez un malus de -20% d'HP")
+    .addField("Coups spéciaux", "?CS Liche")
     .setImage("https://i.imgur.com/ZaA9j6e.jpg")
     message.channel.sendMessage(help_embed);
 }
@@ -44,16 +48,17 @@ if(message.content === préfix + "Lézard"){
     .addField("Lézard", "Vous êtes un Lézard, une espèce qui en vaut le coup d'oeil")
     .addField("Bonus", "Vous avec un bonus de 10% d'HP (simple calcule rapide à faire, n'hésitez pas à contacter le staff si vous y arrivez pas il n'y a aucune honte !)")
     .addField("Malus", "Vous ne pouvez pas vous équiper d'armure")
+    .addField("Coups spéciaux", "?CS Lézard")
     .setImage("https://vignette.wikia.nocookie.net/overlordmaruyama/images/7/72/Overlord_II_EP03_101.png/revision/latest?cb=20180127052134")
     message.channel.sendMessage(help_embed);
 }
-
 if(message.content === préfix + "Automaton"){
     var help_embed = new Discord.RichEmbed()
     .setColor("#7c7c70")
     .addField("Automaton", "Vous êtes un Automate, vous avez le corps d'une machine à l'apparence humaine")
     .addField("Bonus", "Vous êtes immunisé au saignement et au poison")
     .addField("Malus", "Vous devez obligatoirement avoir un maître (joueur) et sa vie passe avant la votre")
+    .addField("Coups spéciaux", "?CS Automaton")
     .setImage("https://vignette.wikia.nocookie.net/overlordmaruyama/images/3/35/Overlord_III_EP01_011.png/revision/latest?cb=20180713084418")
     message.channel.sendMessage(help_embed);
 }
@@ -64,6 +69,7 @@ if(message.content === préfix + "Elf"){
     .addField("Elf", "Vous êtes un Elf, Vous êtes une race plutôt rare")
     .addField("Bonus", "Vous avez un bonus de 10% de mana (simple calcule rapide à faire, n'hésitez pas à contacter le staff si vous y arrivez pas il n'y a aucune honte !)")
     .addField("Malus", "Vous avez un malus de 10% d'HP")
+    .addField("Coups spéciaux", "?CS Elf")
     .setImage("https://vignette.wikia.nocookie.net/overlordmaruyama/images/0/07/Overlord_EP01_097.png/revision/latest?cb=20150713021025")
     message.channel.sendMessage(help_embed);
 }
@@ -75,6 +81,7 @@ if(message.content === préfix + "Nymphe"){
     .setDescription("Vous êtes la représentation de ce qui pourrait se rapprocher à une déesse féminine. Vous êtes la plus belle")
     .addField("Bonus", "10% de mana en plus et la capacité de faire rentrer en état de veille votre corps, venant à créer un esprit contrôlable qui peut se battre et qui a 1 HP")
     .addField("Malus", "Vous êtes obligatoirement une femme et votre éspèce est grandement recherché pour être vendue au marcher noir \n moins 20% d'HP")
+    .addField("Coups spéciaux", "?CS Nymphe")
     .setImage("https://media.discordapp.net/attachments/534758707561496580/585846279977041948/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.webp?width=373&height=560")
     message.channel.sendMessage(help_embed);
 }
@@ -85,6 +92,7 @@ if(message.content === préfix + "Démon"){
     .addField("Démon", "Vous êtes un Démon, vous avez une forme humanoïde bien que certain trait de votre apparence peuvent être différents que celui d'un humain")
     .addField("Bonus", "Vous avez un bonus de 5% d'HP et de mana")
     .addField("Malus", "Les sorts de soins vous infliges des dégâts doublés")
+    .addField("Coups spéciaux", "?CS Démon")
     .setImage("https://www.manga-news.com/public/images/vols/overlord-light-novel-12-jp.jpg")
     message.channel.sendMessage(help_embed);
 }
@@ -96,6 +104,7 @@ if(message.content === préfix + "Beastman"){
     .setDescription("Vous êtes un homme ou une femme qui avait des ressemblances physiques à un animal")
     .addField("Bonus", "Vous avez un bonus unique à votre race précise (à voir avec le staff, par exemple ''Suivre les traces de pas jusqu'à 2 chanels''")
     .addField("Malus", "Vous avez donc également un malus unique (à voir avec le staff, par exemple ''ne sais pas nager''")
+    .addField("Coups spéciaux", "?CS Beastman")
     .setImage("https://media.discordapp.net/attachments/536292650698014731/585798353187635210/thumb-1920-834932.png?width=936&height=585")
     message.channel.sendMessage(help_embed);
 }
@@ -479,10 +488,8 @@ if(message.content === préfix + "Blocage rapide"){
 }
 
 
-
-
-
-
-
+/// Coups spéciaux race palier 1 ///
+/// Coups spéciaux race palier 1 ///
+/// Coups spéciaux race palier 1 ///
 
 
