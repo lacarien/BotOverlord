d2e8b1877750c5a86c4a09a3e8e663e7dc376de4
lacarien <46967709+lacarien@users.noreply.github.com>
2019-12-29 09:31:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,21 @@ client.on("message",message =>{
     const args = message.content.slice(préfix.length).split(' ');
     const command = args.shift().toLowerCase();
 
-    if(command === 'Test:'){
-        if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-        
-        message.channel.send(`Vous executez un violent coups : ${args}`);
-    }
     
+
     if (command === 'args-info') {
         if (!args.length) {
             return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
     
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
+
+    if(command === 'Test:'){
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        
         message.channel.send(`Command name: ${command}\nArguments: ${args}`);
     }
 
