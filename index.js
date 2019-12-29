190ef9f32dc83c74c40b08ce3f80bb55c76b7302
lacarien <46967709+lacarien@users.noreply.github.com>
2019-12-29 09:13:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ client.login(process.env.TOKENS);
 var préfix = "?";
  
 
-    const command = args.shift().toLowerCase();
+   
 
 client.on("message",message =>{
 
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+    
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
   
 
      /*eval(fs.readFileSync(__dirname + '/combat.js')+'');
