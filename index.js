9b7a56482edb36467034de9272dcfa47d5f2d67d
lacarien <46967709+lacarien@users.noreply.github.com>
2019-12-29 09:34:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on("message",message =>{
         message.channel.send(`Command name: ${command}\nArguments: ${args}`);
     }
 
-    if(command === 'test'){
+    if(command === 'test :'){
         if (!args.length) {
             return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
