07d78ffbb245f05b81c28fbd71128dd479737d71
lacarien <46967709+lacarien@users.noreply.github.com>
2019-12-30 21:04:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -143,9 +143,9 @@
             if(message.content === préfix + "Liste d'attaque"){
                 var y = new Discord.RichEmbed()
                 .addField("**===================================**", "**===================================**")
-                .addField("**Voleur**", "LVL 1 : ?Coup-dague [Vos dégats] + ?Esquive [Dégats reçu]\nLVL 5 ?VBlocage [Dégats reçu]\n LVL10 Evolution ?Assassin")
+                .addField("**Voleur**", "LVL 1 : ?Coup-dague [Vos dégats] + ?Esquive [Dégats reçu]\nLVL 5 ?Blocage [Dégats reçu]\n LVL10 Evolution ?Assassin")
                 .addField("**épéiste**", "LVL 1 : ?Coup-epee [Vos dégats] + ?Esquive [Dégats reçu]\nLVL 2 ?Blocage [Dégats reçu]\n LVL10 Evolution ?Chevalier")
-                .addField("**Paladin**", "LVL 1 : ?Coup-masse [Vos dégats] + ?PBlocage [Dégats reçu]\nLVL 5 ?Esquive [Dégats reçu]\n LVL10 Evolution ?Juggernaut")
+                .addField("**Paladin**", "LVL 1 : ?Coup-masse [Vos dégats] + ?Blocage [Dégats reçu]\nLVL 5 ?Esquive [Dégats reçu]\n LVL10 Evolution ?Juggernaut")
                 .addField("**===================================**", "**===================================**")
                 .addField("**Info pour les esquives", "Si vous avez 0 points en agilité : Esquive [dégats reçu]\n Si vous en avez 1 au total : Super_esquive [dégats reçu]\n Si 2 : Mega_esquive [dégats reçu]\n Si 3 : Supra_esquive [dégats reçu]\n Et si 4 : Divin_esquive [dégats_reçu]")
                 .setColor("#ff4600")
