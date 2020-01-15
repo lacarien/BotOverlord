a20bf9eb1c453c6f56b1fbf0f010a463b5ffd53e
lacarien <46967709+lacarien@users.noreply.github.com>
2020-01-15 14:46:11 +0100

    Update Item.js

--- Item.js
+++ Item.js
@@ -26,7 +26,7 @@
             var result = new Discord.RichEmbed() 
             .setColor("#898989")
             .setImage("https://cdn.discordapp.com/attachments/641778159464742942/664036348952313866/weapons20swords20celtic20albion_www.png")
-            .addField("Vous achetez une dague en fer", ":crossed_swords: 1")
+            .addField("Vous achetez une dague en fer",  ":crossed_swords: 1")
             if(result < 80)
             var result = new Discord.RichEmbed()
             .setColor("#898989")
@@ -77,11 +77,10 @@
             }     
 
 
-if(message.content === préfix + "Peaux de crocro"){
+if(message.content === préfix + "Peau de crocro"){
     var help_embed = new Discord.RichEmbed()
     .setColor("#00148f")
-    .addField("Location du loot", "Crocro\nChemin du palier 1")
-    .addField("Achat", "?")
+    .addField("**Crocro du palier 1**", " :fire: **Localisation : Plaine chaude\n:dollar: **Prix d'achat : 20 Mercure**\n:moneybag: **Prix de revente : 10 Mercure**\n:third_place: **Appentissage du metier couturier :** ?Couture crocro")
     .setImage("https://lh4.googleusercontent.com/-0X3phCZZsfo/Tz4X2duAU_I/AAAAAAAAGEM/LLdJ5aqGzIw/Cape%20de%20l%27Homme%20Ours.png")
     .setFooter("©[LCR] Production", "https://cdn.discordapp.com/attachments/625441285578162177/641778421277392907/art-romance-of-the-apocalypse-romantically-apocalyptic-comics-sunglasses-leather-jacket-mask-pilot.jpg")
     message.channel.sendMessage(help_embed);
