9e5d884f1986d4c207c146eaa9e714b0cb01c96d
lacarien <46967709+lacarien@users.noreply.github.com>
2020-01-19 14:03:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@
 
 
 
-        if(message.content === préfix + "Help"){
+        if(message.content === préfix + "Help" || message.content === préfix + "help" ){
             var y = new Discord.RichEmbed()
             .addField(":book:  **?Histoire**", "Ici vous aurez un résumé du thème du serveur")
             .addField(":man_detective: **?Classe**", "Ici vous aurez simplement la liste des classes lors de la création de votre personnage")
@@ -200,7 +200,7 @@
         }   
         if(message.content === préfix + "Elu"){
             var y = new Discord.RichEmbed()
-            .addField("__**Elu**__", ":trident: **Bonus :** +20% d'armure magique\n:fleur_de_lis: **Malus :** -20% d'armure\n:notepad_spiral: **Description :** : Les Elus, une race que l'on pourrait comparer au ange et cela est le cas cependant les Elu sont née de parents humains ou de parents Elu ou d'un parents Humains et d'un parent Elu, ayant comme principale fonction de jouer les intermédiaires entre  les hommes et les démons à fin de protéger les humains de cette race corrompu il voue à leurs naissance une grande méfiance envers les démons ne pouvant donc jamais leurs faire confiance à 100% même si ils deviennent amis.")
+            .addField("__**Elu**__", ":trident: **Bonus :** +20% d'armure magique\n:fleur_de_lis: **Malus :** -10% d'attaque\n:notepad_spiral: **Description :** : Les Elus, une race que l'on pourrait comparer au ange et cela est le cas cependant les Elu sont née de parents humains ou de parents Elu ou d'un parents Humains et d'un parent Elu, ayant comme principale fonction de jouer les intermédiaires entre  les hommes et les démons à fin de protéger les humains de cette race corrompu il voue à leurs naissance une grande méfiance envers les démons ne pouvant donc jamais leurs faire confiance à 100% même si ils deviennent amis.")
             .setImage("https://cdn.discordapp.com/attachments/661225907578601472/661227221226356746/73696359_p0_master1200.jpg")
             .setColor("#ffffff")
             .setFooter("©[LCR] Production", "https://cdn.discordapp.com/attachments/625441285578162177/641778421277392907/art-romance-of-the-apocalypse-romantically-apocalyptic-comics-sunglasses-leather-jacket-mask-pilot.jpg");
@@ -208,7 +208,7 @@
         } 
         if(message.content === préfix + "Automate"){
             var y = new Discord.RichEmbed()
-            .addField("__**Automate**__", ":trident: **Bonus :** Immunisé au poison\n:fleur_de_lis: **Malus :** -20% d'armure\n:notepad_spiral: **Description :** : Race créer par les humains, ce sont des êtres mécanique fait à partir de métal et animé par la magie, cette race est cependant considéré comme hérésie pour les croyants les plus extrêmes car les automates ne sont pas l'œuvre de dieu et que seul dieu à le pouvoir de donner la vie et qu'en créant un automate cela voudrait dire qu'on essaye de se faire passer pour dieu, leurs créations date d'il y a des millénaire et la façon d'en créer à était perdu voir même oublié de rare schéma sont trouvable  mais même avec cela il est très rare d'arriver à donner la vie à un Automate même en y consacrant toute sa vie certain n'y arrive pas ")
+            .addField("__**Automate**__", ":trident: **Bonus :** -50%  de dmg sur les changements d'états \n:fleur_de_lis: **Malus :** -10% d'attaque\n:notepad_spiral: **Description :** : Race créer par les humains, ce sont des êtres mécanique fait à partir de métal et animé par la magie, cette race est cependant considéré comme hérésie pour les croyants les plus extrêmes car les automates ne sont pas l'œuvre de dieu et que seul dieu à le pouvoir de donner la vie et qu'en créant un automate cela voudrait dire qu'on essaye de se faire passer pour dieu, leurs créations date d'il y a des millénaire et la façon d'en créer à était perdu voir même oublié de rare schéma sont trouvable  mais même avec cela il est très rare d'arriver à donner la vie à un Automate même en y consacrant toute sa vie certain n'y arrive pas ")
             .setImage("https://danbooru.donmai.us/data/__cz2128_delta_overlord_drawn_by_hh_101__3a474d29abe339371e5edf2fbdd421bb.jpg?download=1")
             .setColor("#717171")
             .setFooter("©[LCR] Production", "https://cdn.discordapp.com/attachments/625441285578162177/641778421277392907/art-romance-of-the-apocalypse-romantically-apocalyptic-comics-sunglasses-leather-jacket-mask-pilot.jpg");
@@ -216,7 +216,7 @@
         }
         if(message.content === préfix + "Démon"){
             var y = new Discord.RichEmbed()
-            .addField("__**Démon**__", ":trident: **Bonus :** +20% d'armure magique\n:fleur_de_lis: **Malus :** -10% d'attaque\n:notepad_spiral: **Description :** :  Des êtres maléfique . . . enfin d'après les saintes écritures car même si une grande majorité d'entre eux sont d'une nature maléfique ayant de mauvaise intention envers les autres races une petites partie d'entre eux ne sont cependant pas comme sa et peuvent tout aussi gentil que n'importe qui")
+            .addField("__**Démon**__", ":trident: **Bonus :** +10% d'attaque\n:fleur_de_lis: **Malus :** -20% d'armure magique\n:notepad_spiral: **Description :** :  Des êtres maléfique . . . enfin d'après les saintes écritures car même si une grande majorité d'entre eux sont d'une nature maléfique ayant de mauvaise intention envers les autres races une petites partie d'entre eux ne sont cependant pas comme sa et peuvent tout aussi gentil que n'importe qui")
             .setImage("https://media.discordapp.net/attachments/661225907578601472/661227290587299870/60340737_p0_master1200.jpg?width=1183&height=559")
             .setColor("#a90000")
             .setFooter("©[LCR] Production", "https://cdn.discordapp.com/attachments/625441285578162177/641778421277392907/art-romance-of-the-apocalypse-romantically-apocalyptic-comics-sunglasses-leather-jacket-mask-pilot.jpg");
