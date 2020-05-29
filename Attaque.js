fc30c1f16c3578bee0d9b2f3e8af0eab5eb76b80
lacarien <46967709+lacarien@users.noreply.github.com>
2020-05-29 13:32:29 +0200

    Update Attaque.js

--- Attaque.js
+++ Attaque.js
@@ -11,13 +11,6 @@ if(message.content === préfix + "Bonus voleur" || message.content === préfix +
     .setImage("https://i.pinimg.com/originals/5d/ff/3a/5dff3a4218e9cd05ac9e54da8f73741e.jpg")
     message.channel.send(y);
 }
-if(message.content === préfix + "Bonus paladin" || message.content === préfix + "bonus Paladin" || message.content === préfix + "bonus paladin"  || message.content === préfix + "Bonus Paladin"){
-    var y = new Discord.RichEmbed()
-    .addField("**__Du Lvl 15 à 30 pour chaque level__**", "**+1AT** :crossed_swords: \n**+10HP** :heartpulse:")
-    .setImage("https://images.alphacoders.com/522/thumb-1920-522947.jpg")
-    message.channel.send(y);
-}
-
 if(message.content === préfix + "Bonus mage" || message.content === préfix + "bonus Mage" || message.content === préfix + "bonus mage"  || message.content === préfix + "Bonus Mage"){
     var y = new Discord.RichEmbed()
     .addField("**__Du Lvl 1 à 15 pour chaque level__**", "**+1AT** :crossed_swords: \n**+10Mana** :cyclone:")
@@ -30,4 +23,57 @@ if(message.content === préfix + "Bonus ranger" || message.content === préfix +
     .setImage("https://stmed.net/sites/default/files/fantasy-archer-wallpapers-28134-4369491.jpg")
     message.channel.send(y);
 }
+if(message.content === préfix + "Bonus chevalier" || message.content === préfix + "Bonus chevalier"){
+    var y = new Discord.RichEmbed()
+    .addField("**__Du Lvl 16 à 30 pour chaque level__**", "**+0.5AR** :shield: \n**+7.5HP** :heartpulse:\n**+1AT** :crossed_swords:\n**+2.5** :cyclone:")
+    .setColor("#1a00ff")
+    .setImage("https://i.pinimg.com/originals/51/1a/f4/511af4ad91018f2dd5c4f2e88ebc10ce.jpg")
+    message.channel.send(y);
+}
+if(message.content === préfix + "Bonus Paladin" || message.content === préfix + "Bonus paladin"){
+    var y = new Discord.RichEmbed()
+    .addField("**__Du Lvl 16 à 30 pour chaque level__**", "**+0.5AR** :shield: \n**+10HP** :heartpulse:\n**+0.5AT** :crossed_swords:\n**+2.5** :cyclone:")
+    .setColor("#1a00ff")
+    .setImage("https://i.pinimg.com/originals/6b/59/7d/6b597db4ed653727e7ccd0fc19667e76.jpg")
+    message.channel.send(y);
+}
+if(message.content === préfix + "Bonus Assassin" || message.content === préfix + "Bonus assassin"){
+    var y = new Discord.RichEmbed()
+    .addField("**__Du Lvl 16 à 30 pour chaque level__**", "**+0.5AR** :shield: \n**+2.5HP** :heartpulse:\n**+2AT** :crossed_swords:\n**+2.5** :cyclone:")
+    .setColor("#ff0000")
+    .setImage("https://i.pinimg.com/originals/54/3a/df/543adf167fbf500be19e947aaf2cf5bd.jpg")
+    message.channel.send(y);
+}
+if(message.content === préfix + "Bonus Archer" || message.content === préfix + "Bonus archer"){
+    var y = new Discord.RichEmbed()
+    .addField("**__Du Lvl 16 à 30 pour chaque level__**","**+5HP** :heartpulse:\n**+2AT** :crossed_swords:\n**+5** :cyclone:")
+    .setColor("#019521")
+    .setImage("https://i.pinimg.com/originals/fe/d3/e0/fed3e092a7ae1d845fcb1d3ca464726f.jpg")
+    message.channel.send(y);
+}
+if(message.content === préfix + "Bonus Archer" || message.content === préfix + "Bonus archer"){
+    var y = new Discord.RichEmbed()
+    .addField("**__Du Lvl 16 à 30 pour chaque level__**","**+5HP** :heartpulse:\n**+2AT** :crossed_swords:\n**+5** :cyclone:")
+    .setColor("#019521")
+    .setImage("https://i.pinimg.com/originals/fe/d3/e0/fed3e092a7ae1d845fcb1d3ca464726f.jpg")
+    message.channel.send(y);
+}
+if(message.content === préfix + "Bonus Sorcier" || message.content === préfix + "Bonus sorcier"){
+    var y = new Discord.RichEmbed()
+    .addField("**__Du Lvl 16 à 30 pour chaque level__**","**+5HP** :heartpulse:\n**+1AT** :crossed_swords:\n**+10** :cyclone:")
+    .setColor("#00ffe7")
+    .setImage("https://i.pinimg.com/originals/7b/03/50/7b03505857ea0835c3d8b82d0c78a351.jpg")
+    message.channel.send(y);
+}
+if(message.content === préfix + "Bonus Druid" || message.content === préfix + "Bonus druid"){
+    var y = new Discord.RichEmbed()
+    .addField("**__Du Lvl 16 à 30 pour chaque level__**","**+5HP** :heartpulse:\n**+1AT** :crossed_swords:\n**+10** :cyclone:")
+    .setColor("#8aff61")
+    .setImage("https://media.discordapp.net/attachments/712345553895424011/715667957836939435/White.Mage.Final.Fantasy.full.172221.jpg?width=397&height=560")
+    message.channel.send(y);
+}
+
+
+
+
 
