7892345ce5b4fb09242bdc431ff0ca8a30ef244d
lacarien <46967709+lacarien@users.noreply.github.com>
2020-06-09 15:58:14 +0200

    Update quete.js

--- quete.js
+++ quete.js
@@ -126,15 +126,15 @@ if(message.content === préfix + "Quête argent" || message.content === préfix 
 
 if(message.content === préfix + "Quête argent accomplie"){
     var help_embed = new Discord.RichEmbed()
-    var min = Math.floor(Math.min(15));
-    var max = Math.floor(Math.max(20)); 
+    var min = Math.floor(Math.min(10));
+    var max = Math.floor(Math.max(15)); 
     var y = Math.floor(Math.random() * (max - min + 1) ) + min;
     var help_embed = new Discord.RichEmbed()
-    var mins = Math.floor(Math.min(2));
-    var maxs = Math.floor(Math.max(5)); 
+    var mins = Math.floor(Math.min(1));
+    var maxs = Math.floor(Math.max(3)); 
     var x = Math.floor(Math.random() * (maxs - mins + 1) ) + mins;
     var y = new Discord.RichEmbed()
-    .addField("Gain de la récompense de la quête :exclamation: ",  ":third_place:  Pièce(s) de bronze :" + x + "\n:second_place: Pièce(s) d'argent :" + y)
+    .addField("Gain de la récompense de la quête :exclamation: ",  ":third_place:  Pièce(s) de bronze :" + y + "\n:second_place: Pièce(s) d'argent :" + x)
     .setColor("#9e9a9a")
     message.channel.sendMessage(y);
 }
@@ -143,35 +143,38 @@ if(message.content === préfix + "Quête argent accomplie"){
 
 if(message.content === préfix + "Quêtes rang Or"){
     var result = Math.floor((Math.random() * 100) + 1);
-    if(result < 35)
+    if(result < 40)
     var help_embed = new Discord.RichEmbed()
-    .setTitle("L'ultime combattant'")
-    .setDescription("Tuer un Lézard primitif seul")
+    .setTitle("Un petit pas pour l'homme...Mais un grand pas pour l'aventurier")
+    .setDescription("Tuer deux monstre de la fôret")
+    .setImage("https://images-ext-2.discordapp.net/external/pXQsej33bjF3UVMqUjWoOF8wRe49N1prCDBe9b_JW2c/https/i.pinimg.com/originals/b0/07/6c/b0076c06dc93229a327b95fc1a9a477f.jpg?width=996&height=560")
     .setColor("#000000")
-    else if(result < 70)
+    else if(result < 80)
     var help_embed = new Discord.RichEmbed()
-    .setTitle("Garde provisoire")
-    .setDescription("Sauver une personne en difficulté(qui a ses HP en dessous de 90%)")
+    .setTitle("Garde de terrain")
+    .setDescription("Vous devez patrouiller au ruisseau pendant 2 heurs et de si possible capturer une race néfaste à l'humanité.  Mais attention...Un sorcier de la garde vous surveille à travers son miroir.")
+    .setImage("https://besthqwallpapers.com/Uploads/3-7-2018/58159/thumb2-fire-keeper-dark-souls-3-action-art-dark-souls.jpg")
     .setColor("#000000")
     else if (result < 101)
     var help_embed = new Discord.RichEmbed()
-    .setTitle("Explorateur")
-    .setDescription("Vous devez tuer 2 monstres de la forêt mystique")
+    .setTitle("Un vraie aventurier !")
+    .setDescription("Vous devez faire le donjon des ruines !")
+    .setImage("https://images-ext-1.discordapp.net/external/ZwLJolXYLGt5k4Njh01KxLqPPXjR6kUC1OHLi9PGO0k/https/i.pinimg.com/originals/ac/de/e6/acdee674d76b7863f1844d493a352314.jpg?width=996&height=560")
     .setColor("#000000")
     message.channel.sendMessage(help_embed);
 }
 
 if(message.content === préfix + "Quête rang Or accomplie"){
     var help_embed = new Discord.RichEmbed()
-    var min = Math.floor(Math.min(80));
-    var max = Math.floor(Math.max(120)); 
+    var min = Math.floor(Math.min(0));
+    var max = Math.floor(Math.max(1)); 
     var y = Math.floor(Math.random() * (max - min + 1) ) + min;
     var help_embed = new Discord.RichEmbed()
-    var mins = Math.floor(Math.min(20));
-    var maxs = Math.floor(Math.max(30)); 
+    var mins = Math.floor(Math.min(15));
+    var maxs = Math.floor(Math.max(20)); 
     var x = Math.floor(Math.random() * (maxs - mins + 1) ) + mins;
-    var minss = Math.floor(Math.min(0));
-    var maxss = Math.floor(Math.max(2)); 
+    var minss = Math.floor(Math.min(3));
+    var maxss = Math.floor(Math.max(4)); 
     var q = Math.floor(Math.random() * (maxss - minss + 1) ) + minss;
     var y = new Discord.RichEmbed()
     .addField("Gain de la récompense de la quête :exclamation: ",  ":third_place:  Pièce(s) de bronze: " + x + "\n :second_place:  Pièce(s) d'argent: " + q + "\n XP dans le rang Or: " + y )
