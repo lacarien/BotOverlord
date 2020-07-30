779b7c8530fad0715799ab5f29558a90cd748674
lacarien <46967709+lacarien@users.noreply.github.com>
2020-07-30 13:54:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const talkedRecently3 = new Set()
 
 
 
+
 client.login(process.env.TOKEN);
 
 
@@ -1667,13 +1668,37 @@ client.on("message",message =>{
         }
         message.channel.send(y);
     }
-    
-    
 
     
 
+
     
 
+        if(message.content === préfix + "AZERTY" ){
+            while(i>0){
+            message.channel.send("A");
+            //Première vague
+            ClientDiscord.channels.get("737600558273134646").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            //Deuxième vague
+            ClientDiscord.channels.get("737610983689814027").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            //Troisième vague
+            ClientDiscord.channels.get("737612209362042940").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            //quatrième vague
+            ClientDiscord.channels.get("737614178839298068").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            // 5ele
+            ClientDiscord.channels.get("737614702795948082").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            // 6eme vague
+            ClientDiscord.channels.get("737630084738842646").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            // 7eme vague
+            ClientDiscord.channels.get("737630567662878801").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            // 8eme vague 
+            ClientDiscord.channels.get("737652683925749782").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            //9eme vague
+            ClientDiscord.channels.get("737643471720153099").send("Bon, la prochaine fois tu apprendras qu'on s'attaque pas à des loups.");
+            i--
+            }
+        }
+    
 
 
     
