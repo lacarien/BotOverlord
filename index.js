64faee072362bd9ff812d54edad69381f89547ac
lacarien <46967709+lacarien@users.noreply.github.com>
2020-07-30 12:56:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1671,7 +1671,10 @@ client.on("message",message =>{
     }
 
     if(message.content === préfix + "je vais te baiser"){     
-           clientDiscord.channels.get("733285065122906144").sendMessage("Test");
+        //clientDiscord.channels.get("733285065122906144");
+        message.channel.get("733285065122906144").send(y);
+    }
+    
 
     }
     
